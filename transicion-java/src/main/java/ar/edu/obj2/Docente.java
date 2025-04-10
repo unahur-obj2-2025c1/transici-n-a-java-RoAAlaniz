864ebd13c0e93014d913ad7182bc56ef4f46d0b8
RoAAlaniz <rocioabril.alaniz@estudiantes.unahur.edu.ar>
2025-04-10 20:31:32 -0300

    Inicio clase Docente.

--- transicion-java/src/main/java/ar/edu/obj2/Docente.java
+++ transicion-java/src/main/java/ar/edu/obj2/Docente.java
@@ -1,5 +1,16 @@
 package ar.edu.unahur.obj2;
 
 public class Docente extends Persona{
+    
+    private Integer cantCursos = 0;
 
+    @Override 
+    public Integer inteligencia(){
+        return super.inteligencia() + (2 * cantCursos)
+    }
+
+    @Override 
+    public Integer esDestacado(){
+        return cantCursos > 3;
+    }
 }
