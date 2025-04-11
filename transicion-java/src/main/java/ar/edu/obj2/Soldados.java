53714b69285840b92734193407cc3b4c91cfef1f
RoAAlaniz <rocioabril.alaniz@estudiantes.unahur.edu.ar>
2025-04-11 01:19:45 -0300

    Inicio clase Soldado.

---
+++ transicion-java/src/main/java/ar/edu/obj2/Soldados.java
@@ -0,0 +1,15 @@
+package ar.edu.obj2;
+
+public class Soldado extends Persona{
+
+    private List<Arma> armas;
+
+    public Integer potencia(){
+        return super.potencia() + armas.stream().mapToInt(arma -> arma.potencia())sum();
+    }
+
+    @Override 
+    public void ofrecerTributo(Paneta planeta){
+        planeta.construirMurallas(km:5);
+    }
+}
